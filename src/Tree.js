7da88b5af408b437c842cfd5a0c3c8476d69d659
highqualitycode <priceofjs@gmail.com>
2021-08-10 08:38:01 -0500

    fix: scrolling issue

--- src/Tree.js
+++ src/Tree.js
@@ -80,7 +80,7 @@ export default class Tree extends React.Component {
             rowHeight={this._cache.rowHeight}
             rowRenderer={this.measureRowRenderer(nodes)}
             width={width || autoWidth}
-            scrollTop={scrollToIndex}
+            scrollToIndex={scrollToIndex}
             scrollToAlignment={scrollToAlignment}
           />
         )}
