bf04c71ce6fb11ebe1a201cabb9b9b5ce04f0094
HighQualityCode <priceofjs@gmail.com>
2022-05-17 19:22:30 -0700

    fix: close btn issue

--- src/FilteringContainer.js
+++ src/FilteringContainer.js
@@ -76,7 +76,7 @@ export default class FilteringContainer extends React.Component {
       onSelectedGroupChange,
       indexSearch,
       searchPlaceholder = 'Search...',
-      renderCloseBtn = () => null,
+      renderCloseBtn,
     } = this.props;
 
     const relevantNodes =
@@ -99,7 +99,17 @@ export default class FilteringContainer extends React.Component {
               this.filterInput = input;
             }}
           />
-          {!!filterText && renderCloseBtn()}
+          {!!filterText && (
+            <div onClick={this.handleFilterTextClear} className="close-btn">
+              {renderCloseBtn ? (
+                renderCloseBtn()
+              ) : (
+                <svg class="close-icon" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+                  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                </svg>
+              )}
+            </div>
+          )}
           {groups && <GroupRenderer groups={groups} selectedGroup={selectedGroup} onChange={onSelectedGroupChange} />}
         </div>
         {treeRenderer({nodes: filteredNodes, nodeParentMappings})}
